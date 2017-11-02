54c94404eab12e60d8259941a312fb95d71f0c61
Brian McIlwain <brianwmcilwain@gmail.com>
2017-11-02 19:27:05 +0000

    additional improvement to setup script

--- setup.sh
+++ setup.sh
@@ -44,6 +44,6 @@ git config --global user.email "bmcilw1@gmail.com"
 
 # Get .configs setup
 cd ~
-git clone https://github.com/bmcilw1/configs.git ~ && cp -r ~/configs/. . && rm -rf ~/configs
+git clone https://github.com/bmcilw1/configs.git ~ && cp -r ~/configs/. ~/. && rm -rf ~/configs
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugInstall
