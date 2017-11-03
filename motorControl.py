77e0857012b8d6b1a57dc8315135181114078526
Brian McIlwain <bmcilw1@gmail.com>
2017-11-03 17:52:38 +0000

    Broke up pin configs from reset of setup

--- motorControl.py
+++ motorControl.py
@@ -8,6 +8,8 @@ UART.setup("UART1") # P9_24
 START_SIG = "\xAA"
 CHAN0 = "\x00"
 CHAN1 = "\x01"
+NUM_OF_TARGETS = "\x02"
+CENTER0 = CENTER1 = 1480
 
 ser = serial.Serial(port = "/dev/ttyO1", baudrate=9600)
 ser.close()
@@ -16,37 +18,67 @@ ser.open()
 if ser.isOpen():
 	print "Sending commands..."
 
-def SetTarget(channel, target, device_id="\x0C", cmd_set_target = "\x04"):
+def SetTarget(channel, target, device_id="\x0C", cmd = "\x04"):
     # Protocol setup
     ser.write(START_SIG)
     ser.write(device_id)
-    ser.write(cmd_set_target)
+    ser.write(cmd)
     ser.write(channel)
     ser.write(target)
 
+def SetMultiTarget(target0, target1, device_id="\x0C", cmd = "\x1F"):
+    # Protocol setup
+    ser.write(START_SIG)
+    ser.write(device_id)
+    ser.write(cmd)
+    ser.write(NUM_OF_TARGETS)
+    ser.write(CHAN0)
+    ser.write(target0)
+    ser.write(target1)
+
+
 def DriveStraight(speed):
-    target0 = 1485 + speed # 1485 micro seconds is median
+    target0 = CENTER0 + speed # 1485 micro seconds is median
     target0 = int(target0 * 4) # convert to quarter micro-seconds
     target0= ''.join([chr(target0 & 0x7F), chr((target0 >> 7) & 0x7F)])
-    SetTarget(CHAN0, target0)
     
-    target1 = 1485 - speed # 1485 micro seconds is median
+    target1 = CENTER1 - speed # 1485 micro seconds is median
     target1 = int(target1 * 4) # convert to quarter micro-seconds
     target1= ''.join([chr(target1 & 0x7F), chr((target1 >> 7) & 0x7F)])
-    SetTarget(CHAN1, target1)
     
+    SetMultiTarget(target0, target1)
+    
+def TurnInPlace(degree, speed):
+    target0 = CENTER0 + speed # 1485 micro seconds is median
+    target0 = int(target0 * 4) # convert to quarter micro-seconds
+    target0= ''.join([chr(target0 & 0x7F), chr((target0 >> 7) & 0x7F)])
+    
+    target1 = CENTER1 + speed # 1485 micro seconds is median
+    target1 = int(target1 * 4) # convert to quarter micro-seconds
+    target1= ''.join([chr(target1 & 0x7F), chr((target1 >> 7) & 0x7F)])
+    
+    SetMultiTarget(target0, target1)
+    time.sleep(3)
+    DriveStraight(0)
 
-DriveStraight(20)
-time.sleep(2)
-DriveStraight(500)
-time.sleep(.3)
-DriveStraight(0)
-time.sleep(.5)
-DriveStraight(-20)
-time.sleep(2)
-DriveStraight(-500)
-time.sleep(.3)
-DriveStraight(0)
+def TestTurnInPlace():
+    TurnInPlace(90, 50)
+
+def TestDriveStraight():
+    DriveStraight(20)
+    time.sleep(2)
+    DriveStraight(500)
+    time.sleep(.3)
+    DriveStraight(0)
+    time.sleep(.5)
+    DriveStraight(-20)
+    time.sleep(2)
+    DriveStraight(-500)
+    time.sleep(.3)
+    DriveStraight(0)
+    
+TestTurnInPlace()
+#TestDriveStraight()
 
 ser.close()
 print "Commands send. Connection closed."
--- setup.sh
+++ setup.sh
@@ -1,46 +1,14 @@
 #!/bin/bash
 
-# Configure appropiate ports as PWM
-config-pin -q p9.14
-config-pin p9.14 pwm
-config-pin -q p9.14
-
-config-pin -q p9.16
-config-pin p9.16 pwm
-config-pin -q p9.16
-
-config-pin -q p8.19
-config-pin p8.19 pwm
-config-pin -q p8.19
-
-config-pin -q p8.13
-config-pin p8.13 pwm
-config-pin -q p8.13
-
-# Configure UART1
-config-pin -q p9.26
-config-pin p9.26 uart # RX
-config-pin -q p9.26
-
-config-pin -q p9.24
-config-pin p9.24 uart # TX
-config-pin -q p9.24
-
-# Configure UART2
-config-pin -q p9.22
-config-pin p9.22 uart # RX
-config-pin -q p9.22
-
-config-pin -q p9.21
-config-pin p9.21 uart # TX
-config-pin -q p9.21
-
 # Get pyserial plugin
-sudo apt-get install python-serial
+if [ $1=="p" ]
+then
+    sudo apt-get install python-serial
+fi
 
 # Git config
-if [ $1="b" ]
-    then
+if [ $1=="b" ]
+then
     git config --global user.name "Brian McIlwain"
     git config --global user.email "bmcilw1@gmail.com"
     
