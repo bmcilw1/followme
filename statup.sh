276b9c501ce84c8d6ae424b3c1a60c762bc55f5b
Brian McIlwain <bmcilw1@gmail.com>
2017-11-07 17:56:58 +0000

    Added startup script

---
+++ statup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Configure appropiate port types
+if [ -f /var/lib/cloud9/followme/configPins.sh ]; then
+    source /var/lib/cloud9/followme/configPins.sh
+fi
+
+# Run specified script
+if [ -f /var/lib/cloud9/followme/motorControl.py ]; then
+    sudo python /var/lib/cloud9/followme/motorControl.py
+fi
