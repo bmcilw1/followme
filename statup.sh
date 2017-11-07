fa7775cfbb0f1ff7bc9966f7f20805aeb701f70c
Brian McIlwain <bmcilw1@gmail.com>
2017-11-07 18:28:12 +0000

    Hacky fix for race condition on startup pin configs

--- statup.sh
+++ statup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Prevent race condition on pin-config
+sleep 1m
+
 # Configure appropiate port types
 if [ -f /var/lib/cloud9/followme/configPins.sh ]; then
     source /var/lib/cloud9/followme/configPins.sh
