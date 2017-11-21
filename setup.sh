e732fa630967e8c84a6eff2660d746a50e8e6205
Demo User <debian@beaglebone.localdomain>
2017-11-21 04:37:52 +0000

    Fixed setup.sh service

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Update packages
+sudo apt-get update
+
 # Get pyserial plugin
 sudo apt-get install python-serial
 
 # Set up service script to begin on restart
+if [ ! -f /etc/systemd/senior_design.service ]; then
+	ln /var/lib/cloud9/followme/senior_design.service /etc/systemd/senior_design.service
+fi
+
 sudo systemctl enable senior_design.service
 sudo systemctl start senior_design.service
