3df157bbf841fd8653b69710834a1e11e390b008
Demo User <debian@beaglebone.localdomain>
2017-11-21 04:15:14 +0000

    Removed logic from setup script

--- setup.sh
+++ setup.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
 # Get pyserial plugin
-if [ $1=="p" ]
-then
-    sudo apt-get install python-serial
-fi
+sudo apt-get install python-serial
 
-# Git config
-if [ $1=="b" ]
-then
-    git config --global user.name "Brian McIlwain"
-    git config --global user.email "bmcilw1@gmail.com"
-    
-    # Get .configs setup
-    cd ~
-    git clone https://github.com/bmcilw1/configs.git ~ && cp -r ~/configs/. ~/. && rm -rf ~/configs
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    vim -c PlugInstall
-fi
+# Set up service script to begin on restart
+sudo systemctl enable senior_design.service
+sudo systemctl start senior_design.service
