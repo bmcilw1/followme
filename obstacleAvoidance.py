5544a67fe4ffe923721ae8b8316f9f239a083099
Brian McIlwain <brianwmcilwain@gmail.com>
2017-11-22 03:58:45 +0000

    Cleaned up code

--- obstacleAvoidance.py
+++ obstacleAvoidance.py
@@ -9,11 +9,17 @@ from time import sleep
 
 ADC.setup()
 
-usonic="P9_40"
-irL="P9_39"
-irR="P9_38"
-irD="P9_37"
+# Sensor pins
+U_SONIC="P9_40"
+IR_L="P9_39"
+IR_R="P9_38"
+IR_D="P9_37"
 
+# Sensor distance thresholds
+CLIFF_DELTA = .1
+COLLISION_THRESHOLD = .2 # in meters
+
+# Motor controller UART
 UART.setup("UART1") # P9_24
 
 # Motor contoller constants
@@ -82,41 +88,84 @@ def TurnInPlace(degree, speed=50, sec_for_90_degree=2.9):
     time.sleep(abs(degree)/90.0*sec_for_90_degree)
     DriveStraight(0)
 
+def readUS(pin):
+    usonicVal=ADC.read(pin)
+    usonicVolt=usonicVal*1.8 # Convert to voltage
+    # linear response
+    distanceMetersUS = usonicVolt*8 # convert to meters via manual tuning
+    return distanceMetersUS
+
+def readIR(pin, usDist):
+    irVal=ADC.read(pin)
+    irVolt=irVal*1.8 # Convert to voltage
+    
+    # Convert to meters via manual tuning
+    # Prevent divide by zero error add small delta
+    # This equation only works for 1-5.5 meters distance
+    distanceMetersIR = 1/(irVolt + .00000001)
+    return distanceMetersIR
+    
+def avgArray(a):
+    return reduce(lambda x, y: x + y, a) / float(len(a))
+        
 # Smooth readings by taking the average value of several readings
-usDist = [0, 0, 0, 0, 0]
-irLDist = [0, 0, 0, 0, 0]
-i = 0;
+# Circular arrays to implement averaging
+usDist = [0, 0, 0, 0, 0, 0, 0, 0]
+irLDist = [0, 0, 0, 0, 0, 0, 0, 0]
+irRDist = [0, 0, 0, 0, 0, 0, 0, 0]
+irDDist = [0, 0, 0, 0, 0, 0, 0, 0]
+startCtr = 0
+i = 0
 
 while(True):
-        usonicVal=ADC.read(usonic)
-        usonicVolt=usonicVal*1.8 # Convert to voltage
-        #print "The ultrasonic voltage is: ", usonicVolt
-        #distanceInches = usonicVolt / 0.0075 # convert to inches via manual tuning
-        # linear response
-        distanceMetersUS = usonicVolt*8 # convert to meters via manual tuning
-        usDist[i] = distanceMetersUS
-        #print "There is an object ", distanceMetersUS, "meters away US."
+    usDist[i] = readUS(U_SONIC)
+    #print "There is an object ", usDist[i], "meters away US."
+    
+    # the avgDist to the nearest obstacle straight ahead
+    avgDistUS = avgArray(usDist)
+    #print "There is an object ", avgDist, "meters away."
+    
+    irLDist[i] = readIR(IR_L, avgDistUS)
+    #print "There is an object ", irLDist[i], "meters away LIR."
+    irRDist[i] = readIR(IR_R, avgDistUS)
+    #print "There is an object ", irRDist[i], "meters away RIR."
+    
+    # Will generally be less than a meter
+    irDDist[i] = readIR(IR_D, .05)
+    #print "There is an object ", irDDist[i], "meters away DIR."
+    
+    # The avgDist for the old and new half of the down IR readings
+    # If there is a sufficiently large gap assume cliff
+    halfIrDDist = len(irDDist)/2
+    avgDistDIROld = avgArray(irDDist[: halfIrDDist])
+    avgDistDIRNew = avgArray(irDDist[halfIrDDist :])
+    
+    cliff = True if abs(avgDistDIRNew - avgDistDIROld) > CLIFF_DELTA else False
         
-        irLVal=ADC.read(irL)
-        irLVolt=irLVal*1.8
-        
-        #print "The left IR voltage is: ", irLVolt
-        # TODO: find formula for non-linear response
-        distanceMetersIRL = 1/irLVolt # convert to meters via manual tuning
-        irLDist[i] = distanceMetersIRL
-        #print "There is an object ", distanceMetersIRL, "meters away LIR."
-        
-        # the avgDist to the nearest obstacle straight ahead
-        avgDist = reduce(lambda x, y: x + y, usDist) / float(len(usDist))
-        #print "There is an object ", avgDist, "meters away."
-        
-        if avgDist < .1:
-            TurnInPlace(45);
-        else:
-            DriveStraight(500);
-        
-        i = 0 if len(usDist) else i + 1
-        sleep(.001)
+    if startCtr < len(usDist) - 1:
+        # Wait until sufficent samples are taken
+        DriveStraight(0)
+    elif cliff:
+        # Avoid cliff: back up and turn around
+        # Check for this FIRST
+        DriveStraight(-100)
+        sleep(1)
+        TurnInPlace(-180)
+        startCtr = 0
+    elif avgDistUS < COLLISION_THRESHOLD:
+        # Avoid standing obstacle: choose the right
+        TurnInPlace(45)
+        startCtr = 0
+    else:
+        # We're free to go
+        DriveStraight(700)
+    
+    # Increment counters
+    i = 0 if i == len(usDist) - 1 else i + 1
+    startCtr = startCtr if startCtr == len(usDist) - 1 else startCtr + 1
+    
+    # Delay until next cycle
+    sleep(.0001)
 
 # Stop
 DriveStraight(0)
--- obstacleDetection.py
+++ obstacleDetection.py
@@ -25,61 +25,58 @@ startCtr = 0
 i = 0
 
 def readUS(pin):
-        usonicVal=ADC.read(pin)
-        usonicVolt=usonicVal*1.8 # Convert to voltage
-        # linear response
-        distanceMetersUS = usonicVolt*8 # convert to meters via manual tuning
-        return distanceMetersUS
+    usonicVal=ADC.read(pin)
+    usonicVolt=usonicVal*1.8 # Convert to voltage
+    # linear response
+    distanceMetersUS = usonicVolt*8 # convert to meters via manual tuning
+    return distanceMetersUS
 
 def readIR(pin, usDist):
-        irVal=ADC.read(pin)
-        irVolt=irVal*1.8 # Convert to voltage
+    irVal=ADC.read(pin)
+    irVolt=irVal*1.8 # Convert to voltage
         
-        # Convert to meters via manual tuning
-        # Prevent divide by zero error add small delta
-        # This equation only works for 1-5.5 meters distance
-        distanceMetersIR = 1/(irVolt + .0000001)
-        return distanceMetersIR
+    # Convert to meters via manual tuning
+    # Prevent divide by zero error add small delta
+    # This equation only works for 1-5.5 meters distance
+    distanceMetersIR = 1/(irVolt + .0000001)
+    return distanceMetersIR
 
 while(True):
-        usDist[i] = readUS(U_SONIC)
-        print "There is an object ", usDist[i], "meters away US."
+    usDist[i] = readUS(U_SONIC)
+    print "There is an object ", usDist[i], "meters away US."
+    
+    # the avgDist to the nearest obstacle straight ahead
+    avgDistUS = reduce(lambda x, y: x + y, usDist) / float(len(usDist))
+    #print "There is an object ", avgDist, "meters away."
+    
+    irLDist[i] = readIR(IR_L, avgDistUS)
+    print "There is an object ", irLDist[i], "meters away LIR."
+    irRDist[i] = readIR(IR_R, avgDistUS)
+    #print "There is an object ", irRDist[i], "meters away RIR."
+    
+    # Will always be less than a meter
+    irDDist[i] = readIR(IR_D, .05)
+    print "There is an object ", irDDist[i], "meters away DIR."
+    # The avgDist for the old and new half of the down IR readings
+    # If there is a sufficiently large gap assume cliff
+    halfIrDDist = len(irDDist)/2
+    avgDistDIROld = reduce( lambda x, y: x + y, irDDist[: halfIrDDist] ) / float(halfIrDDist)
+    avgDistDIRNew = reduce( lambda x, y: x + y, irDDist[halfIrDDist :] ) / float(halfIrDDist)
+    
+    #print "avgDistDIROld ", avgDistDIROld
+    #print "avgDistDIRNew ", avgDistDIRNew
+    cliff = True if abs(avgDistDIRNew - avgDistDIROld) > CLIFF_DELTA else False
+    
+    if startCtr < len(usDist) - 1:
+        print "startup"
+    elif avgDistUS < COLLISION_THRESHOLD:
+        print "stop"
+    elif cliff:
+        print "cliff"
+        startCtr = 0
+    else:
+        print "go"
         
-        # the avgDist to the nearest obstacle straight ahead
-        avgDistUS = reduce(lambda x, y: x + y, usDist) / float(len(usDist))
-        #print "There is an object ", avgDist, "meters away."
-        
-        irLDist[i] = readIR(IR_L, avgDistUS)
-        print "There is an object ", irLDist[i], "meters away LIR."
-        
-        irRDist[i] = readIR(IR_R, avgDistUS)
-        #print "There is an object ", irRDist[i], "meters away RIR."
-        
-        # Will always be less than a meter
-        irDDist[i] = readIR(IR_D, .05)
-        print "There is an object ", irDDist[i], "meters away DIR."
-        
-        # The avgDist for the old and new half of the down IR readings
-        # If there is a sufficiently large gap assume cliff
-        halfIrDDist = len(irDDist)/2
-        avgDistDIROld = reduce( lambda x, y: x + y, irDDist[: halfIrDDist] ) / float(halfIrDDist)
-        avgDistDIRNew = reduce( lambda x, y: x + y, irDDist[halfIrDDist :] ) / float(halfIrDDist)
-        
-        #print "avgDistDIROld ", avgDistDIROld
-        #print "avgDistDIRNew ", avgDistDIRNew
-        cliff = True if abs(avgDistDIRNew - avgDistDIROld) > CLIFF_DELTA else False
-        
-        if startCtr < len(usDist) - 1:
-            print "startup"
-        elif avgDistUS < COLLISION_THRESHOLD:
-            print "stop"
-        elif cliff:
-            print "cliff"
-            startCtr = 0
-        else:
-            print "go"
-        
-        # J is startup counter
-        i = 0 if i == len(usDist) - 1 else i + 1
-        startCtr = startCtr if startCtr == len(usDist) - 1 else startCtr + 1
-        sleep(.5)
+    i = 0 if i == len(usDist) - 1 else i + 1
+    startCtr = startCtr if startCtr == len(usDist) - 1 else startCtr + 1
+    sleep(.5)
